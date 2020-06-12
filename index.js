f874d5c4b915d81d536c9e120f16c5ac28f053c6
ShahmeerLXIV <66737084+ShahmeerLXIV@users.noreply.github.com>
2020-06-12 18:00:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ bot.on('message', message=>{
         case 'zahando':
             if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('```You do not have permission to use this command.```')
             message.delete();
-            message.channel.send('https://tenor.com/view/scrape-okuyasu-nijimura-za-hando-the-hand-jojo-gif-14165071')
-            .then(msg => msg.delete(1000));
             if(!args[1]) return message.channel.send('```Please specify a value.```')
-            message.channel.bulkDelete(args[1]);   
+            message.channel.bulkDelete(args[1]);  
+            message.channel.send('https://tenor.com/view/scrape-okuyasu-nijimura-za-hando-the-hand-jojo-gif-14165071')
+            .then(message => message.delete(5000)) 
             break;
         case 'heavensdoor':
             const embed = new Discord.MessageEmbed()
