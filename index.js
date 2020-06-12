ab8ccf3570410bccf3e7de1fd04c400b2b3beb0e
ShahmeerLXIV <66737084+ShahmeerLXIV@users.noreply.github.com>
2020-06-12 19:00:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,97 +5,102 @@ const token = (process.env.token);
 
 const Prefix = ('^');
 
-bot.on('ready', () =>{
+bot.on('ready', () => {
     Console.log('Bruh Momento');
 })
 
-bot.on('message', message=>{
-    let args = message.content.substring(Prefix.length).split(" ");
-    
-    switch(args[0]){
-        case 'ping':
-            message.channel.send("pong")
-            break;
-        case 'info':
-            if(args[1] === 'Shahmeer'){
-                message.channel.send('He is currently hibernating.');
-        
-            }else{
-                message.channel.send('Please state a user')
-            }
-            break;
-        case 'zahando':
-            if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('```You do not have permission to use this command.```')
-            message.delete();
-            if(!args[1]) return message.channel.send('```Please specify a value.```')
-            message.channel.bulkDelete(args[1]); 
-            message.send('https://tenor.com/view/scrape-okuyasu-nijimura-za-hando-the-hand-jojo-gif-14165071').then(msg => msg.delete(3000))
-            break;
-        case 'heavensdoor':
-            const embed = new Discord.MessageEmbed()
-            .setTitle('User Information')
-            .addField('User\'s Name', message.author.username)
-            .addField('Join Date', message.author)
-            .setColor(0x00cae7)
-            .setThumbnail(message.author.displayAvatarURL())
-            message.channel.send(embed);
-            break;
-        case 'kick':
-            if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('```You do not have permission to use this command.```')
+bot.on('message', message => {
+        let args = message.content.substring(Prefix.length).split(" ");
 
-            if(user){                
-            
-                if(member){
-                    member.kick('```You have been kicked.```').then(()=>{
-                        message.channel.send(```Successfully kicked ${user.tag}```)
-                    })
-                    .catch(err =>{
-                        message.channel.send('```I was unable to kick that user.```');
-                        Console.log(err);
-                    });
-                    
-                } else{
-                    message.channel.send('``That user is not in the server.``')
+        switch (args[0]) {
+            case 'ping':
+                message.channel.send("pong")
+                break;
+            case 'info':
+                if (args[1] === 'Shahmeer') {
+                    message.channel.send('He is currently hibernating.');
+
+                } else {
+                    message.channel.send('Please state a user')
                 }
-            } else{
-                message.channel.send('``That user is not in the server``');
-            }
-            break;
-        case 'ban':
-            if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('```You do not have permission to use this command.```')
-            const user = message.mentions.users.first();
+                break;
+            case 'zahando':
+                if (!message.member.hasPermission("ADMINISTRATOR")) {
+                    return message.channel.send('```You do not have permission to use this command.```')}
+                message.delete();
+                if (!args[1]) {
+                    return message.channel.send('```Please specify a value.```')}
+                message.channel.bulkDelete(args[1]);
+                break;
+            case 'heavensdoor':
+                const embed = new Discord.MessageEmbed()
+                    .setTitle('User Information')
+                    .addField('User\'s Name', message.author.username)
+                    .addField('Join Date', message.author)
+                    .setColor(0x00cae7)
+                    .setThumbnail(message.author.displayAvatarURL())
+                message.channel.send(embed);
+                break;
+            case 'kick':
+                if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('```You do not have permission to use this command.```')
 
-            if(user){
-                const member  = message.guild.member(user);
+                if (user) {
 
-                if(member){
-                    member.ban('```You have been banner.```').then(()=>{
-                        message.channel.send(```Successfully banned ${user.tag}```)
-                    })
-                    .catch(err =>{
-                        message.channel.send('```I was unable to ban that user.```')
-                    });
+                    if (member) {
+                        member.kick('```You have been kicked.```').then(() => {
+                                message.channel.send(``
+                                    `Successfully kicked ${user.tag}`
+                                    ``)
+                            })
+                            .catch(err => {
+                                message.channel.send('```I was unable to kick that user.```');
+                                Console.log(err);
+                            });
 
-                } else{
-                    message.channel.send('```That user is not in the server```')
+                    } else {
+                        message.channel.send('``That user is not in the server.``')
+                    }
+                } else {
+                    message.channel.send('``That user is not in the server``');
                 }
-            } else{
-                message.channel.send('```That user is not in the server```');
-            }
-            break;
+                break;
+            case 'ban':
+                if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('```You do not have permission to use this command.```')
+                const user = message.mentions.users.first();
+
+                if (user) {
+                    const member = message.guild.member(user);
+
+                    if (member) {
+                        member.ban('```You have been banner.```').then(() => {
+                                message.channel.send(``
+                                    `Successfully banned ${user.tag}`
+                                    ``)
+                            })
+                            .catch(err => {
+                                message.channel.send('```I was unable to ban that user.```')
+                            });
+
+                    } else {
+                        message.channel.send('```That user is not in the server```')
+                    }
+                } else {
+                    message.channel.send('```That user is not in the server```');
+                }
+                break;
 
 
-        
-            
-            
 
-    
-        
-    }
 
-            
-            
-            
+
+
+
+
+        }
+
+
+
+
     }
 
 )
