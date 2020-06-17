a2101f036b304c5af08d9bbbdde504c0e22ff2f7
ShahmeerLXIV <66737084+ShahmeerLXIV@users.noreply.github.com>
2020-06-17 17:29:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -22,39 +22,39 @@ bot.on('message', message=>{
             message.channel.send("pong")
             break;
         case 'info':
-            if(!args[1]) return message.channel.send('```Please specify a user.```');
+            if(!args[1].toLowerCase) return message.channel.send('```Please specify a user.```');
 
-            if(args[1] === 'Shahmeer' || 'shahmeer' || 'shah'){
+            if(args[1] === 'Shahmeer'){
                 message.channel.send('```He is currently playing anime waifu game.```');
             }
-            if(args[1] === 'Gorodo' || 'gorodo'){
+            if(args[1] === 'Gorodo'){
                 message.channel.send('```Horny.```');
             }
-            if(args[1] === 'Kart' || 'kart'){
+            if(args[1] === 'Kart'){
                 message.channel.send('```He is playing Roblox.```');
             }
-            if(args[1] === 'Starry' || 'starry'){
+            if(args[1] === 'Starry'){
                 message.channel.send('```He is currently very tired.```');
             }
-            if(args[1] === 'Yeetus' || 'yeetus'){
+            if(args[1] === 'Yeetus'){
                 message.channel.send('```He is eating cheetos.```')
             }
-            if(args[1] === 'H' || 'h'){
+            if(args[1] === 'H'){
                 message.channel.send('```It\'s a me Super Paper Mario! WAHOOO!!```');
             }
-            if(args[1] === 'Poke' || 'poke'){
+            if(args[1] === 'Poke'){
                 message.channel.send('```He is currently sucking Hichem\'s dick.```');
             }
-            if(args[1] === 'Shuk' || 'shuk'){
+            if(args[1] === 'Shuk'){
                 message.channel.send('```He controls the future of the PPP server.```');
             }
-            if(args[1] === 'Echoes' || 'echoes'){
+            if(args[1] === 'Echoes'){
                 message.channel.send('```District 6 Female.```');
             }
-            if(args[1] === 'Wons' || 'wons'){
+            if(args[1] === 'Wons'){
                 message.channel.send('```Known child rapist, his most brutal case was raping a cosplayer dressed as a fictional character from the franchise JoJo\'s Bizarre Adventure , the said cosplayer dressed as a character of the fourth part, Shigekiyo "Shigechi" Yangu.```');
             }
-            if(args[1] === 'Hoodied' || 'hoodied'){
+            if(args[1] === 'Hoodied'){
                 message.channel.send('https://imgur.com/aHgzynv');
             }
             break;
