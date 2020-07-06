a68c0c511488b30f7f91bc827f71d18e50c90c19
ShahmeerLXIV <66737084+ShahmeerLXIV@users.noreply.github.com>
2020-07-06 17:03:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = (process.env.token);
+const token = ('NzI5NjYyNjMwNTQ0ODAxNzky.XwMRFA.zCEbyZ7mDWb2TBUhL_PbOmjM-7I');
 
 const Prefix = ('^');
 
@@ -133,4 +133,4 @@ bot.on('message', message=>{
 
 )
 
-bot.login(process.env.token);
+bot.login('NzI5NjYyNjMwNTQ0ODAxNzky.XwMRFA.zCEbyZ7mDWb2TBUhL_PbOmjM-7I');
